e5311bcc914b90533338154f9248df2b5d018f76
rakshitranpariya <rakshitranpariya@gmail.com>
2025-12-27 12:21:22 -0400

    Adding test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+// test('renders the portfolio heading', () => {
+//   render(<App />);
+//   expect(screen.getByRole('heading', { name: /portfolio/i })).toBeInTheDocument();
+// });
+test('renders App without crashing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
+
