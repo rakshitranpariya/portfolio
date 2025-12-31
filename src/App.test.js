c422090db7ac6cbfc2589fba01c955026fefeaf9
rakshitranpariya <rakshitranpariya@gmail.com>
2025-12-30 22:48:23 -0400

    Adding the import statement

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-
+import { HelmetProvider } from "react-helmet-async";
 // test('renders the portfolio heading', () => {
 //   render(<App />);
 //   expect(screen.getByRole('heading', { name: /portfolio/i })).toBeInTheDocument();
